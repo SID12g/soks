4f7776e312bee0da1307794142b146738fb37da5
SID12g <sid12g@naver.com>
2024-05-23 09:58:34 +0900

    fix: a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Signout from "@/components/signout/signout";
 import Signup from "@/components/signup/signup";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
-import { connectDB } from "@/utils/database";
 import Link from "next/link";
+import { connectDB } from "@/utils/database";
 
 export default async function Home() {
   const session: any = await getServerSession(authOptions);
